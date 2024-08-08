82fb3af2085b959d5c394d37ab710ffcb92d79e9
4ndrelim <tiltedaf3@gmail.com>
2024-08-08 11:59:54 +0800

    feat: implement resumption training

---
+++ holograms/horisaki/training_resumption/config.py
@@ -0,0 +1,20 @@
+"""
+For setting file-level configuration
+"""
+import torch
+from torch import nn
+import numpy as np
+
+
+# Ensure the below the same!
+DTYPE_TORCH = torch.float64
+DTYPE_NP = np.float64
+
+IMAGE_SIZE = 64
+
+EPOCHS = 30
+LEARNING_RATE = 0.0012
+LOSS_FN = nn.SmoothL1Loss(reduction='mean', beta=0.18)
+INITIALIZER = lambda tensor: nn.init.kaiming_normal_(tensor, nonlinearity='relu')
+GAMMA = 0.5
+MILESTONES = [10, 20]
